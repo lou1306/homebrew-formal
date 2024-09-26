897859f3efe32c0660d982880ddd4b7c08135fb3
Luca Di Stefano <lucadistefano90@gmail.com>
2024-09-26 10:40:57 +0200

    Add utils/mkbottle.sh, utils/pre-commit
    * mkbottle.sh automates the creation and upload of bottles.
      It requires jq and gh (both on homebrew-core), and only
      works if the user has write access to this repo.
      Please do not bottle ANY formula unless the software is clearly
      licensed as open-source (see https://spdx.org/licenses/).
      When in doubt open an issue *before* uploading any bottles!
    
    * utils/pre-commit: pre-commit hook that runs `brew audit` on all
      staged rb files. Install it by running
      `ln -s ../../utils/pre-commit ./git/hooks`
      from the root of the repository.

---
+++ utils/mkbottle.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## This script requires gh and jq. To obtain them:
+## brew install gh ; brew install jq
+
+gh repo set-default lou1306/homebrew-formal
+
+FORMULA=$1
+VERSION=$(brew info --json lou1306/formal/$FORMULA | jq -r '.[] | .versions.stable')
+
+brew rm lou1306/formal/$FORMULA >/dev/null 2>&1
+brew install --build-bottle lou1306/formal/$FORMULA
+
+## Create bottle files
+brew bottle --json --root-url https://github.com/lou1306/homebrew-formal/releases/download/$FORMULA-$VERSION lou1306/formal/$FORMULA
+
+## Fix bottle.tar.gz filename
+BOTTLE=$(ls $FORMULA*bottle*.tar.gz | head -n1) 
+NEWNAME=$(ls $FORMULA*bottle*.tar.gz | sed 's/--/-/')
+# mv $BOTTLE $NEWNAME
+
+## Create GitHub release
+## Todo: if release exists use gh release upload instead
+gh release create --generate-notes "$FORMULA-$VERSION" "$NEWNAME"
+
+brew bottle --write --merge $FORMULA*$VERSION*.bottle.json
