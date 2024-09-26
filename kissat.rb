acb8cdf5857da36f734d53ffd28c69838daa4ac5
Luca Di Stefano <lucadistefano90@gmail.com>
2024-09-26 10:37:23 +0200

    Fix audit errors

--- kissat.rb
+++ kissat.rb
@@ -3,8 +3,7 @@ class Kissat < Formula
   homepage "http://fmv.jku.at/kissat/"
   url "https://github.com/arminbiere/kissat/archive/refs/tags/rel-4.0.1.tar.gz"
   sha256 "4b41edf12ffa5f8e8b1986e5ad3e0bedb4d34b0ed3ecc7c13362bc7ba0aba66b"
-  license 'MIT'
-
+  license "MIT"
 
   bottle do
     root_url "https://github.com/lou1306/homebrew-formal/releases/download/kissat-4.0.1"
