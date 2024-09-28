4166a8051ab9f03c446cc0e670268bc66a4ff666
Luca Di Stefano <lucadistefano90@gmail.com>
2024-09-28 14:31:56 +0200

    Update utils/mkbottle.sh

--- utils/mkbottle.sh
+++ utils/mkbottle.sh
@@ -6,22 +6,31 @@
 gh repo set-default lou1306/homebrew-formal
 
 FORMULA=$1
-ARCH=$(arch)
 VERSION=$(brew info --json lou1306/formal/$FORMULA | jq -r '.[] | .versions.stable')
 
 brew rm lou1306/formal/$FORMULA >/dev/null 2>&1
-brew install --build-bottle --bottle-arch=$ARCH lou1306/formal/$FORMULA
+brew install --build-bottle lou1306/formal/$FORMULA
 
 ## Create bottle files
 brew bottle --json --root-url https://github.com/lou1306/homebrew-formal/releases/download/$FORMULA-$VERSION lou1306/formal/$FORMULA
 
+
 ## Fix bottle.tar.gz filename
 BOTTLE=$(ls $FORMULA*bottle*.tar.gz | head -n1) 
 NEWNAME=$(ls $FORMULA*bottle*.tar.gz | sed 's/--/-/')
-# mv $BOTTLE $NEWNAME
+mv $BOTTLE $NEWNAME
 
-## Create GitHub release
-## Todo: if release exists use gh release upload instead
-#gh release create --generate-notes "$FORMULA-$VERSION" "$NEWNAME"
 
-brew bottle --write --merge $FORMULA*$VERSION*.bottle.json
+## Create/update GitHub release
+if gh release list | awk '{ print $0 }' | grep $FORMULA-$VERSION; then
+  echo "release exists"
+  gh release upload  "$FORMULA-$VERSION" $NEWNAME --clobber
+else
+  gh release create --generate-notes "$FORMULA-$VERSION" "$NEWNAME"
+fi
+
+brew bottle --write $FORMULA*$VERSION*.bottle.json
+echo "All set!"
+echo "Make sure that brew can resolve"
+echo "the bottle file before submitting the PR :)"
+
