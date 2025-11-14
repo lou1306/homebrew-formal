b22bc9c4209c1497d0188a4e3baa147b59ef0ad4
Luca Di Stefano <lucadistefano90@gmail.com>
2025-11-14 14:20:00 +0100

    Add licensing information

--- mathsat.rb
+++ mathsat.rb
@@ -5,6 +5,7 @@ class Mathsat < Formula
   homepage "http://mathsat.fbk.eu/index.html"
   url "https://mathsat.fbk.eu/release/mathsat-5.6.14-macos.tar.gz"
   sha256 "e839462862dc2abc6975ad84e8cd25a4f854046597d6f73fa7f245ab9ae30b00"
+  license :cannot_represent
 
   depends_on "gmp"
   depends_on "python-setuptools"
@@ -24,7 +25,7 @@ class Mathsat < Formula
     bin.install "bin/mathsat"
     include.install Dir["include/*.h"]
     lib.install "lib/libmathsat.a"
-    (share/"mathsat").install "configurations", "examples"
+    (share/"mathsat").install "configurations", "examples", "LICENSE.txt", "README.txt"
 
     # Compile and install Java library.
     Dir.chdir "java" do
@@ -53,14 +54,20 @@ class Mathsat < Formula
       f.puts("-cp .:#{HOMEBREW_PREFIX}/lib/mathsat.jar \"$@\"")
     end
     bin.install "mathsatj-compile", "mathsatj-run"
+  end
 
-    ohai "Additional scripts are installed to compile and run Java files."
-    puts <<-EOS
+  def caveats
+    <<~EOS
       To compile a Java file Test.java with mathsat, run
         $ mathsatj-compile Test.java
 
       To run the class Test.class, run
         $ mathsatj-run Test
+
+      === LICENSE ===
+      #{Utils.safe_popen_read "cat", "#{HOMEBREW_PREFIX}/share/mathsat/LICENSE.txt"}===============
+
+      The license can be found in #{HOMEBREW_PREFIX}/share/mathsat/LICENSE.txt
     EOS
   end
 end
